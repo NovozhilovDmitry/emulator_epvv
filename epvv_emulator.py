3c9bfcb18a648ed1d7b3c5448f7b34fdbc650d5a
Dmitry <novozhilovdmitry93@gmail.com>
2023-04-22 21:44:33 +0500

    add function for naming converts

--- epvv_emulator.py
+++ epvv_emulator.py
@@ -1,3 +1,16 @@
 # gui with fields: qcombobox - result codes, qlineedit or qlabel with text info for result code
 # dict with result code, where keys - codes, values - info for codes;
 # xml sample or create xml from xsd schemas
+import random
+
+
+def converts_name():
+    """
+    :return: выдает имена формата ###-###-###-###-### в шестнадцатиричной системе для интеграционных конвертов
+    """
+    first_part = str(hex(random.randint(1000000000, 9999999999)))
+    second_part = str(hex(random.randint(10000, 99999)))
+    third_part = str(hex(random.randint(10000, 99999)))
+    fourth_part = str(hex(random.randint(10000, 99999)))
+    fifth_part = str(hex(random.randint(100000000000000, 999999999999999)))
+    return f'{first_part[2:10]}-{second_part[2:6]}-{third_part[2:6]}-{fourth_part[2:6]}-{fifth_part[2:14]}'
